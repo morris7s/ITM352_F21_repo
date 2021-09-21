aba9d9da3c076191d3ea98dcfe1d3de1cc7c1728
morris7s <morriss7@hawaii.edu>
2021-09-21 12:03:33 -1000

    lab6

---
+++ Lab6/Ex1.js
@@ -0,0 +1,8 @@
+var age =  19;
+var fav_num = 7;
+var day_of_birth = 04;
+var month_of_birth = 10;
+
+console.log(age + fav_num / day_of_birth * month_of_birth);
+
+console.log((age + fav_num) / day_of_birth * month_of_birth);
