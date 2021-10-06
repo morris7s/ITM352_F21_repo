e531886e4246d44945550453478874b0c94fcd13
morris7s <morriss7@hawaii.edu>
2021-10-05 15:17:19 -1000

    xxx

--- Lab8/Ex3.js
+++ Lab8/Ex3.js
@@ -1,8 +1,8 @@
 require("./product_data.js");
 var num_products = 5;
-var count = 0;
 
-while(count++ != num_products) {
+
+for(var count = 1; eval("typeof name"+count) != 'undefined'; count++) {
     if(count > num_products/2) {
         console.log(`Don’t ask for anything else!`)
         process.exit();
