c9e887b6bee46978bc4f10897b951f85cc7f0290
morris7s <morriss7@hawaii.edu>
2021-11-08 18:58:41 -1000

    eee

--- Lab13/infoserverexercise2a.js
+++ Lab13/infoserverexercise2a.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
+
 app.all('*', function (request, response, next) {
-    response.send(request.method + ' to path ' + request.path);
+    console.log(request.method + ' to path ' + request.path);
+    next();
 });
 app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
+app.get('/test', function(request, response){
+    response.send('Does /test work? If so, you should see me ...');
+});
---
+++ Lab13/infoserverexercise2c.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.all('*', function (request, response, next) {
+    console.log(request.method + ' to path ' + request.path);
+    next();
+});
+
+app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
+app.get('/test', function(request, response){
+    response.send('Does /test work? If so, you should see me ...');
+});
+app.use(express.static('./public'));
