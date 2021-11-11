bda6733fafba6207f91f1fc0bdd3c2baa3b469ee
morris7s <morriss7@hawaii.edu>
2021-11-11 10:29:35 -1000

    Update infoserverexercise2c.js

--- Lab13/infoserverexercise2c.js
+++ Lab13/infoserverexercise2c.js
@@ -7,7 +7,14 @@ app.all('*', function (request, response, next) {
 });
 
 app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
-app.get('/test', function(request, response){
-    response.send('Does /test work? If so, you should see me ...');
+app.get('/test', function (request, response){
+    response.send('Does /test work? If so, you should see me ...' + request.body);
+ 
+    next();
 });
 app.use(express.static('./public'));
+
+app.post('/process_form', function (request, response){
+    response.send('in POST send');
+    next();
+}); 
