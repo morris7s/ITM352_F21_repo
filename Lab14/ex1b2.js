a9e4c162b4eaea68fa880c25887cafcb382aded3
morris7s <morriss7@hawaii.edu>
2021-11-16 12:04:13 -1000

    ooo

---
+++ Lab14/ex1b2.js
@@ -0,0 +1,8 @@
+//var users_reg_data = require('./user_data.json');
+const fs = require('fs');
+
+
+filename = 'user_data.json';
+var user_data_str = fs.readFileSync(filename, 'utf-8');
+var user_data_obj = JSON.parse(user_data_str);
+console.log(user_data_obj, typeof user_data_obj);
